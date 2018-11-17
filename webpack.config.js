dd7ed3ae604286b72abc67b2bb46f24a5c5d361e
Mischa <zaratul@gmx.de>
2018-11-17 13:18:08 +0100

    initial react setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/tsx/App.tsx',
+    devtool: 'sourcemaps',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            },
+             {test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ]
+    }
+};
