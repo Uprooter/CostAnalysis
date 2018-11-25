b9d56c2f77eb053fab3fdae7b5e20be36d7f44a8
Mischa <zaratul@gmx.de>
2018-11-25 18:20:59 +0100

    Cleanup

--- src/main/tsx/App.tsx
+++ src/main/tsx/App.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom";
 import Main from "./components/Main";
 import Notfound from "./components/Notfound";
 import CostItems from "./components/CostItems";
-
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 export class App extends React.Component<{}, {}> {
