e991d738e3b1ccd2d0a2e3847cb43d260a4050eb
Mischa <zaratul@gmx.de>
2018-12-02 17:16:09 +0100

    Minor optimizations

--- src/main/java/de/mischa/HomeController.java
+++ src/main/java/de/mischa/HomeController.java
@@ -6,18 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = { "", "/", "upload", "detailedclusters" })
 	public String index() {
-		return "index";
-	}
-
-	@RequestMapping(value = "/upload")
-	public String upload() {
-		return "index";
-	}
-
-	@RequestMapping(value = "/detailedclusters")
-	public String detailedClusters() {
 		return "index";
 	}
 }
