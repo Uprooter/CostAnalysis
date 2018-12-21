3b3ef56b7d91609db91c31904db155d53a992093
Mischa <zaratul@gmx.de>
2018-12-21 13:52:00 +0100

    Added average calculation with tests

--- src/main/java/de/mischa/calc/AverageCostsCalculationService.java
+++ src/main/java/de/mischa/calc/AverageCostsCalculationService.java
@@ -1,12 +1,10 @@
 package de.mischa.calc;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.time.YearMonth;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +32,10 @@ public class AverageCostsCalculationService {
 					.collect(Collectors.toList());
 		}
 
+		return calculateResult(relevantItems);
+	}
+
+	AverageCostModel calculateResult(List<CostItem> relevantItems) {
 		AverageCostModel result = new AverageCostModel();
 		result.setFixedCostsMischa(this.calculateMonthlyAverage(relevantItems, CostOwner.MISCHA, CostType.FEST));
 		result.setFixedCostsGesa(this.calculateMonthlyAverage(relevantItems, CostOwner.GESA, CostType.FEST));
@@ -49,7 +51,6 @@ public class AverageCostsCalculationService {
 		result.setDiffMischa(this.calculateDiff(relevantItems, CostOwner.MISCHA));
 		result.setDiffGesa(this.calculateDiff(relevantItems, CostOwner.GESA));
 		result.setTotalDiff(result.getDiffGesa() + result.getDiffMischa());
-
 		return result;
 	}
 
@@ -60,7 +61,7 @@ public class AverageCostsCalculationService {
 				.filter(i -> i.getOwner() == owner)//
 				.filter(i -> i.getAmount() > 0)//
 				.mapToDouble(i -> i.getAmount()).sum();
-		return earnings - costs;
+		return earnings + costs;
 	}
 
 	private List<CostItem> getCostsOnly(List<CostItem> relevantItems, CostOwner owner) {
@@ -70,19 +71,30 @@ public class AverageCostsCalculationService {
 				.collect(Collectors.toList());
 	}
 
-	private double calculateMonthlyAverage(List<CostItem> relevantItems, CostOwner owner, CostType type) {
+	double calculateMonthlyAverage(List<CostItem> relevantItems, CostOwner owner, CostType type) {
 		List<CostItem> costTypeItems = this.getCostsOnly(relevantItems, owner)//
 				.stream().filter(i -> i.getType() == type)//
 				.collect(Collectors.toList());
-		
-		//Map<Integer, List<Double>> monthlyAvera
-		for(CostItem item : costTypeItems)
-		{
-			int month = LocalDate.ofEpochDay(item.getCreationDate().getTime()).getMonthValue();			
-		}
-		return 0;
-	}
-	
 
+		Map<String, Double> monthlySums = getMonthlySums(costTypeItems);
+
+		return monthlySums.entrySet().stream().mapToDouble(e -> e.getValue()).average().orElse(0);
+	}
+
+	Map<String, Double> getMonthlySums(List<CostItem> costTypeItems) {
+		Map<String, Double> monthlySums = new HashMap<>();
+		for (CostItem item : costTypeItems) {
+			int month = LocalDate.ofEpochDay(item.getCreationDate().getTime()).getMonthValue();
+			int year = LocalDate.ofEpochDay(item.getCreationDate().getTime()).getYear();
+			String monthYear = month + "" + year;
+			if (monthlySums.containsKey(monthYear)) {
+				Double oldValue = monthlySums.get(monthYear);
+				monthlySums.put(monthYear, oldValue + item.getAmount());
+			} else {
+				monthlySums.put(monthYear, item.getAmount());
+			}
+		}
+		return monthlySums;
+	}
 
 }
---
+++ src/test/java/de/mischa/calc/AverageCostsCalculationServiceTests.java
@@ -0,0 +1,96 @@
+package de.mischa.calc;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.mischa.model.AverageCostModel;
+import de.mischa.model.CostItem;
+import de.mischa.model.CostOwner;
+import de.mischa.model.CostType;
+
+public class AverageCostsCalculationServiceTests {
+
+	@Test
+	public void testMonthlySums() {
+		List<CostItem> costTypeItems = new ArrayList<CostItem>();
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -100.0));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -50.0));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -100.0));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2019, 9, 1), -100.0));
+
+		Map<String, Double> monthlySums = new AverageCostsCalculationService().getMonthlySums(costTypeItems);
+		Assert.assertThat(monthlySums.keySet().size(), is(3));
+		Assert.assertThat(monthlySums.get("92018"), is(-150.0));
+	}
+
+	@Test
+	public void testMonthlyAverage() {
+		List<CostItem> costTypeItems = new ArrayList<CostItem>();
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -100.0));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -200.0));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2019, 9, 1), -150.0));
+
+		double monthlyAverage = new AverageCostsCalculationService().calculateMonthlyAverage(costTypeItems,
+				CostOwner.MISCHA, CostType.FEST);
+		Assert.assertThat(monthlyAverage, is(-150.0));
+	}
+
+	@Test
+	public void testResult() {
+		List<CostItem> costTypeItems = new ArrayList<CostItem>();
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), 100.0, CostOwner.MISCHA, CostType.GEHALT));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), 100.0, CostOwner.MISCHA, CostType.GEHALT));
+
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -100.0, CostOwner.MISCHA, CostType.FEST));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -200.0, CostOwner.MISCHA, CostType.FEST));
+
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -100.0, CostOwner.MISCHA, CostType.FLEXIBEL));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -50.0, CostOwner.MISCHA, CostType.FLEXIBEL));
+
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), 300.0, CostOwner.GESA, CostType.GEHALT));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), 300.0, CostOwner.GESA, CostType.GEHALT));
+
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -50.0, CostOwner.GESA, CostType.FEST));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -25.0, CostOwner.GESA, CostType.FEST));
+
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 9, 1), -100.0, CostOwner.GESA, CostType.FLEXIBEL));
+		costTypeItems.add(this.createCostItems(LocalDate.of(2018, 10, 1), -120.0, CostOwner.GESA, CostType.FLEXIBEL));
+
+		AverageCostModel calculateResult = new AverageCostsCalculationService().calculateResult(costTypeItems);
+		Assert.assertThat(calculateResult.getFixedCostsMischa(), is(-150.0));
+		Assert.assertThat(calculateResult.getFlexCostsMischa(), is(-75.0));
+		Assert.assertThat(calculateResult.getTotalAverageMischa(), is(-225.0));
+		Assert.assertThat(calculateResult.getDiffMischa(), is(-250.0));
+		
+		Assert.assertThat(calculateResult.getFixedCostsGesa(), is(-37.5));
+		Assert.assertThat(calculateResult.getFlexCostsGesa(), is(-110.0));
+		Assert.assertThat(calculateResult.getTotalAverageGesa(), is(-147.5));
+		Assert.assertThat(calculateResult.getDiffGesa(), is(305.0));
+		
+		Assert.assertThat(calculateResult.getTotalAverageFixedCosts(), is(-187.5));
+		Assert.assertThat(calculateResult.getTotalAverageFlexCosts(), is(-185.0));
+		Assert.assertThat(calculateResult.getTotalDiff(), is(55.0));
+	}
+
+	private CostItem createCostItems(LocalDate localDate, double amount, CostOwner owner, CostType type) {
+		CostItem item = new CostItem();
+		item.setCreationDate(new Date(localDate.toEpochDay()));
+		item.setAmount(amount);
+		item.setOwner(owner);
+		item.setType(type);
+		return item;
+	}
+
+	private CostItem createCostItems(LocalDate localDate, double amount) {
+		return this.createCostItems(localDate, amount, CostOwner.MISCHA, CostType.FEST);
+	}
+
+}
