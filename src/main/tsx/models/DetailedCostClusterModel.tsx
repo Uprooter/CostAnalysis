205684d398431bd0163c37896268177d05eedad5
Mischa <zaratul@gmx.de>
2018-12-30 13:05:10 +0100

    First working version

--- src/main/tsx/models/DetailedCostClusterModel.tsx
+++ src/main/tsx/models/DetailedCostClusterModel.tsx
@@ -1,11 +1,7 @@
 
-import { HalProperty, HalResource } from "hal-rest-client";
+export default class DetailedCostClusterModel {
 
-export default class DetailedCostClusterModel extends HalResource {
-
-    @HalProperty()
     public name: string;
 
-    @HalProperty()
     public cluster: string;
 }
